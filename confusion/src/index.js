c237e0b4a8b30187a4e6a491ecd5b635482484e2
peter-vu <vutuananh.vn.sg@gmail.com>
2021-05-24 23:30:29 +0800

    Add header footer

--- confusion/src/index.js
+++ confusion/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 
 ReactDOM.render(
   <React.StrictMode>

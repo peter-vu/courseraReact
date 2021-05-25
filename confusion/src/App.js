f15f7905540cd30ba87f772b78b2735c9edf84a3
peter-vu <vutuananh.vn.sg@gmail.com>
2021-05-25 09:52:21 +0800

    SPA 1

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
-
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -9,9 +9,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }

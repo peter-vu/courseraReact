5ff66fa13f8d74c06c7026eec9aa841317feefb2
peter-vu <vutuananh.vn.sg@gmail.com>
2021-05-26 17:03:55 +0800

    Redux 1

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -62,7 +62,7 @@ class Main extends Component {
               <Route exact path='/aboutus' component={() => <About leaders={this.props.leaders} />} />
               <Route exact path='/menu' component={() => <Menu dishes={this.props.dishes} />} />
               <Route path='/menu/:dishId' component={DishWithId} />
-              <Route exact path='/contactus' component={Contact} />} />
+              <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
           </Switch>
         </div>
